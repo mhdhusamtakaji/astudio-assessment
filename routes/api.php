41614b70b101e52490218642d0b7c577f1b8f2b9
mhdhusamtakaji <mhdhusamtakaji@gmail.com>
2025-02-14 16:11:25 +0400

    feat: Add Passport auth and RESTful endpoints for all models

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,31 @@
 <?php
 
 use App\Http\Controllers\AttributeController;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TimesheetController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+// -------------------------------------
+// 1. fetch authenticated user route 
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// -------------------------------------
+// 2. Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login',    [AuthController::class, 'login']);
 
-// Attributes
-Route::apiResource('attributes', AttributeController::class);
+// -------------------------------------
+// 3. Protected routes for authentication
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 
-// Projects
-Route::apiResource('projects', ProjectController::class);
+// -------------------------------------
+// 4. Protected REST resources
+Route::apiResource('projects', ProjectController::class)->middleware('auth:api');
+Route::apiResource('timesheets', TimesheetController::class)->middleware('auth:api');
+Route::apiResource('attributes', AttributeController::class)->middleware('auth:api');
